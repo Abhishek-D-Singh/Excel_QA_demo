a61c3f80d00ea9883837634ac2d314ef25df4148
Abhishek Singh <Abhee2630@Gmail.Com>
2023-05-12 13:21:15 +0530

    Update excel_qa.py

--- excel_qa.py
+++ excel_qa.py
@@ -57,7 +57,7 @@ from langchain.llms import OpenAI
 
 """Calling langchain csv agent to pass the csv file for QA"""
 
-agent = create_csv_agent(OpenAI(temperature=0),'csv.csv',verbose=True)
+agent = create_csv_agent(OpenAI(temperature=0),'csv.csv')
 
 agent.run("how many rows are there?")
 
